802676dc5e5d9cce3e265a3553d96294cb3b027f
Wladimir Figuera <70117039+wladi27@users.noreply.github.com>
2021-12-07 11:37:04 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/favicon2.ico',
+    badge: 'images/favicon2.ico'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+const notificationPromise = self.registration.showNotification(title, options);
+event.waitUntil(notificationPromise);
